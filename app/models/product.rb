5ab3e172ab38a413207d88b9bc03157efbf5bb41
nsouthard <nathan.southard@gmail.com>
2017-05-09 21:35:39 -0500

    End of night save 5/9/2017

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,23 @@
 class Product < ApplicationRecord
+
+  def friendly_updated_at
+    updated_at.strftime('%b %d, %Y')
+  end
+
+  def sale_message
+    if price < 35  
+      p 'Discount Item!'
+    else 
+      p 'Everyday Value!!'
+    end
+  end 
+
+  def tax
+    price * 0.09
+  end
+
+  def total_cost
+    price + tax
+  end
+
 end
